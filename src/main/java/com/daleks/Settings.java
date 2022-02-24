1c1851d66f536db893fd340909efce7f2e77c447
Iren Attano <79834812+dalekskaro@users.noreply.github.com>
2022-02-24 19:05:21 +0700

    наконец-то открывает приложение

--- src/main/java/com/daleks/Settings.java
+++ src/main/java/com/daleks/Settings.java
@@ -15,7 +15,7 @@ public class Settings implements WebDriverProvider {
         capabilities.setCapability("automationName", "UIAutomator2");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName", "Nexus 5 API 29");
-        capabilities.setCapability("app", "D:\\Downloads\\Wildberries_v.4.5.2000(60470)(7.0-12.0)(arm7a,arm64-8a,x86,x86-64).apk");
+        capabilities.setCapability("app", "C:\\Users\\Ira\\Downloads\\Wildberries.apk");
         capabilities.setCapability("appActivity","ru.wildberries.SplashActivity");
         capabilities.setCapability("appPackage","com.wildberries.ru");
         capabilities.setCapability("fullReset","True");
